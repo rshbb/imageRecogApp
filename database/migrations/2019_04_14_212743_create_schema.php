a76b5029aff039a61d4b31e93f1ca1cb6b24ebf2
rshbb <rfactor78@gmail.com>
2019-04-15 03:12:10 +0530

    added schemas by migration

---
+++ database/migrations/2019_04_14_212743_create_schema.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSchema extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+        Schema::create('USERS', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("name");
+            $table->string("password",20);
+            $table->timestamps();
+        });
+        Schema::create("DATA",function (Blueprint $table){
+            $table->increments("id");
+            $table->integer("uid");
+            $table->string("filePath");
+            $table->text("result");
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(){
+        Schema::dropIfExists('USERS');
+        Schema::dropIfExists("DATA");
+    }
+}
